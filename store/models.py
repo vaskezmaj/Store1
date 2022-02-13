1673039adb6fb968bf4e8e4c333158af656a8938
Aleksandar Vasic <av20191089@student.fon.bg.ac.rs>
2022-02-13 15:09:24 +0100

    Initial commit

--- store/models.py
+++ store/models.py
@@ -35,16 +35,14 @@ class Customer(models.Model):
 
     ]
 
-    first_name = models.CharField(max_length=255)
+    given_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
-    class Meta:
-        db_table = 'store_customer'
-        indexes = [models.Index(fields=['last_name', 'first_name'])]
+
 
 
 class Order(models.Model):
